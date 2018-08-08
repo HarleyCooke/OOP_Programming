a43f58be68d33dfb05ccb5baf2040b26b0128d9f
harleycooke <harley.c@hotmail.com>
2018-08-08 17:21:44 +1000

    Update

--- UnitTest/UnitTestFibonacci/Properties/FiboTestClass.cs
+++ UnitTest/UnitTestFibonacci/Properties/FiboTestClass.cs
@@ -5,7 +5,7 @@ namespace UnitTestFibonacci.Properties
     public class FiboTestClass
     {
         
-        int FibocacciRecTest(int num1 = 0, int num2 = 1, int numFibs = 100) {
+        int FibocacciRec(int num1 = 0, int num2 = 1, int numFibs = 100) {
             
             if (numFibs > 0) {
                 
@@ -13,10 +13,10 @@ namespace UnitTestFibonacci.Properties
                 
                 num2 = num1 + num2;
                 
-                return FibocacciRecTest(num2, num2 + num1, numFibs);
+                return FibocacciRec(num2, num2 + num1, numFibs);
             } 
             
-            if (numFibs != 0)
+            if (numFibs != 99)
             {
                 throw new InvalidOperationException();
             }
