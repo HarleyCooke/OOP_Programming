c6de72c6b1e2d1c9b421364211aa9c8ea1662df3
harleycooke <harley.c@hotmail.com>
2018-08-13 17:01:11 +1000

    Update

---
+++ Problem_42/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+
+namespace Problem_42
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // text = "Words.Txt" File.
+            
+            const string text = "Words.txt";
+            
+                List<string> t = new List<string>();
+                using (StreamReader n = new StreamReader(text))
+                {
+                    string indent;
+                    
+                        while ((indent = n.ReadLine()) !=null)
+                        {
+                            var Words = indent.Replace("\"", "").Split(',');
+
+                                foreach (var word in Words)
+                                {
+                                    
+                                }
+                            
+                            
+                        }
+
+                }
+            
+           
+            
+           
+        }
+    }
+}
